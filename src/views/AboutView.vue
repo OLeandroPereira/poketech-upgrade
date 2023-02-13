3bedc6d0ae3fae09e331ba7dae35003decf0f2c1
Leandro Pereira <https://github.com/OLeandroPereira/site-vue.git>
2023-02-13 14:55:28 -0300

    ajuste no about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,9 +5,9 @@
       <div class="card-body p-3">
         <p>Recebi um teste tecnico para ser entregue em dois dias, na hora foi um susto, porque eu não sabia se conseguiria entregar no prazo que foi solicitado. Mas com fé e coragem fui pra cima, com toda força e dedicação, foram horas de estudos e madrugadas adentro para poder desenvolver o projeto.</p>
 
-        <p>Infelizmente não consegui finalizar a parte de busca do API, onde só consegui fazer a busca da imagem e figura, mas mesmo assim entreguei imcompleto. Mesmo que eu não continue no processo seletivo, foi de grande avalia tudo que aprendi nesses dois dias, evolui em coisas que anteriormente eu tinha dificuldade, descobri que desenvolver com <strong>VUE </strong>não é um bicho de sete cabeças, e senti mais confiança para fazer novos projetos utilizando o framework.</p>
+        <p>Infelizmente não consegui finalizar a parte de busca do API, onde só consegui fazer a busca do nome e imagem, mas mesmo assim entreguei. Mesmo que eu não continue no processo seletivo, foi de grande avalia tudo que aprendi nesses dois dias, evolui em coisas que anteriormente eu tinha dificuldade, descobri que desenvolver com <strong>VUE </strong>não é um bicho de sete cabeças, mas requer muito estudo e dedicação, me senti mais confiante para fazer novos projetos utilizando o framework.</p>
 
-        <p>Eu clonei o meu projeto do desafio, e continue tentando resolver a dificuldade que eu estava tendo sobre o consumo do API. E pra minha surpresa, consegui resolver em pouco tempo, mexendo e testando sem parar até eu entender onde eu estava errando, agora meu projeto encontra-se finalizado, e o que posso dizer é que estou orgulhoso de mim mesmo, pela minha persistência e dedicação para resolver o problema onde eu estava tendo dificuldade, e por sentir mais confiança em mim para desenvolver novos projetos.</p>
+        <p>Eu clonei o meu projeto do desafio, e continuei tentando resolver o problema de consumo do API. E pra minha surpresa, consegui resolver em pouco tempo, mexendo e testando sem parar até eu entender onde eu estava errando, agora meu projeto encontra-se finalizado, e o que posso dizer é que estou orgulhoso de mim mesmo, pela minha persistência e dedicação para resolver o problema onde eu estava tendo dificuldade.</p>
       </div>
     </div>
   </div>
